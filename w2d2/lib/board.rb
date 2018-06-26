6b6dc41147354ea861436bba5cbfc1772429d862
Richard Ma <rich.maaa@gmail.com>
2018-06-26 10:01:10 -0700

    Modified make_move in roboard.rb

--- w2d2/lib/board.rb
+++ w2d2/lib/board.rb
@@ -4,8 +4,8 @@ class Board
   def initialize(name1, name2)
     @cups = Array.new(14){Array.new}
     place_stones
-    @player1 = name1
-    @player2 = name2
+    @name1 = name1
+    @name2 = name2
     @current_player_name = name1
   end
 
@@ -23,27 +23,28 @@ class Board
   def make_move(start_pos, current_player_name)
     stones_in_hand = @cups[start_pos].dup
     @cups[start_pos] = []
-    i = 1
+    curr_pos = start_pos
     until stones_in_hand.empty?
-      i = i % @cups.length
-      # if (start_pos+1)==6 && @current_player_name != @player1
-      #   i += 1
-      #   next
-      # end
-      # if (start_pos+1)==13 && @current_player_name != @player2
-      #   i += 1
-      #   next
-      # end
-      p @cups[start_pos+i]
-      p stones_in_hand
-      @cups[start_pos + i] << stones_in_hand.shift
-      i += 1 unless stones_in_hand.empty?
+      curr_pos += 1
+      curr_pos = curr_pos % @cups.length
+      if curr_pos==6 && @current_player_name==@name1
+        @cups[curr_pos] << stones_in_hand.shift
+      elsif curr_pos==13 && @current_player_name==@name2
+        @cups[curr_pos] << stones_in_hand.shift
+      else
+        @cups[curr_pos] << stones_in_hand.shift
+      end
     end
     render
-    return :switch if @cups[start_pos+ i].length == 1
+    next_turn(curr_pos)
   end
 
   def next_turn(ending_cup_idx)
+    p "ending cup is"
+    p ending_cup_idx
+    return :prompt if ending_cup_idx == 6 && @current_player_name == @name1
+    return :prompt if ending_cup_idx == 13 && @current_player_name == @name2
+    return :switch if @cups[ending_cup_idx].length == 1
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
