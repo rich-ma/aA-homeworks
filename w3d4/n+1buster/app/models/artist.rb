b75c253c092a673b83226dc3fd2c7b31a6b843fc
Richard Ma <rich.maaa@gmail.com>
2018-07-05 07:14:58 -0700

    Add w3d4 homework

---
+++ w3d4/n+1buster/app/models/artist.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+    albums = self.albums.includes(:tracks)
+
+    track_count = {}
+
+    albums.each do |album|
+      track_count[album] = album.tracks.length
+    end
+    track_count
+  end
+end
